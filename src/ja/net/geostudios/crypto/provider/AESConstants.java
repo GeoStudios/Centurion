8d4f7874bfd80c16a72d087cf5528e788037471a
M4ximumPizza <MaximumUltimateDestroyer@gmail.com>
2023-06-12 07:57:55 -0700

    Java Cryto Import

---
+++ src/ja/net/geostudios/crypto/provider/AESConstants.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 GeoStudios and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ja.net.geostudios.crypto.provider;
+
+/**
+ * This class defines the constants used by the AES implementation.
+ *
+ * @author Logan Abernathy
+ * @since Alpha CDK-1.0
+ *
+ * @see com.sun.crypto.provider.AESCipher
+ */
+
+interface AESConstants {
+
+    // AES block size in bytes.
+    int AES_BLOCK_SIZE = 16;
+
+    // Valid AES key sizes in bytes.
+    // NOTE: The values need to be listed in an *increasing* order
+    // since DHKeyAgreement depends on this fact.
+    int[] AES_KEYSIZES = { 16, 24, 32 };
+}
---
+++ src/ja/net/geostudios/crypto/provider/BlowfishParameters.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2023 GeoStudios and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ja.net.geostudios.crypto.provider;
+
+import java.io.*;
+import java.security.AlgorithmParametersSpi;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidParameterSpecException;
+
+/**
+ * This class implements the parameter (IV) used with the Blowfish algorithm in
+ * feedback-mode. IV is defined in the standards as follows:
+ *
+ * <pre>
+ * IV ::= OCTET STRING  -- 8 octets
+ * </pre>
+ *
+ * @author Logan Abernathy
+ * @since Alpha CDK-1.0
+ */
+
+public final class BlowfishParameters extends AlgorithmParametersSpi {
+
+    private com.sun.crypto.provider.BlockCipherParamsCore core;
+
+    public BlowfishParameters() {
+        core = new com.sun.crypto.provider.BlockCipherParamsCore
+                (com.sun.crypto.provider.BlowfishConstants.BLOWFISH_BLOCK_SIZE);
+    }
+    protected void engineInit(AlgorithmParameterSpec paramSpec)
+            throws InvalidParameterSpecException {
+        core.init(paramSpec);
+    }
+
+    protected void engineInit(byte[] encoded)
+            throws IOException {
+        core.init(encoded);
+    }
+
+    protected void engineInit(byte[] encoded, String decodingMethod)
+            throws IOException {
+        core.init(encoded, decodingMethod);
+    }
+
+    protected <T extends AlgorithmParameterSpec>
+    T engineGetParameterSpec(Class<T> paramSpec)
+            throws InvalidParameterSpecException {
+        if (AlgorithmParameterSpec.class.isAssignableFrom(paramSpec)) {
+            return core.getParameterSpec(paramSpec);
+        } else {
+            throw new InvalidParameterSpecException
+                    ("Inappropriate parameter Specification");
+        }
+    }
+
+    protected byte[] engineGetEncoded() throws IOException {
+        return core.getEncoded();
+    }
+
+    protected byte[] engineGetEncoded(String encodingMethod)
+            throws IOException {
+        return core.getEncoded();
+    }
+
+    protected String engineToString() {
+        return core.toString();
+    }
+}
